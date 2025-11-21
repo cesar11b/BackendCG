1879c115d86f014f8ca50186e2cb8397e179f503
Leandro Ramirez <leandromarceloramirez@gmail.com>
2025-11-21 14:52:08 -0600

    agregue las colecciones que faltaban

---
+++ src/api/info-config/routes/info-config.ts
@@ -0,0 +1,7 @@
+/**
+ * info-config router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::info-config.info-config');
---
+++ src/api/info-config/services/info-config.ts
@@ -0,0 +1,7 @@
+/**
+ * info-config service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::info-config.info-config');
